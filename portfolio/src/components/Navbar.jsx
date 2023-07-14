d85719a0f8f53f54e44b11ab64256ce1cc90e8da
INAKHE ISRAEL <israelinakhe@gmail.com>
2023-07-14 11:19:36 +0100

    Init

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import {logo} from '../assets/index'
-import { Link } from 'react-router-dom'
-
-
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { logo } from '../assets/index';
 
 const Navbar = () => {
   return (
-     <div className="w-[1280px] border-b-[1px] h-[142px] bg-blue-500 flex flex-row">
-      <div>
-          <img className='w-[28px]' src={logo} alt='logo'  />
-        <ul>
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/about">
-            <li>About</li>
-          </Link>
-          <Link to="/projects">
-            <li>Projects</li>
-          </Link>
-          <Link to="/contact">
-            <li>Contact</li>
-          </Link>
-        </ul>  
+    <Router>
+      <div className="w-[1280px] border-b-[1px] h-[142px] bg-blue-500 flex flex-row">
+        <div>
+          <img className="w-[28px]" src={logo} alt="logo" />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-     </div>
-  )
+    </Router>
+  );
 }
 
-export default Navbar
+export default Navbar;
